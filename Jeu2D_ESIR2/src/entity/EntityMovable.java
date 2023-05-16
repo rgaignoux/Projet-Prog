0fd5ad07982b0dfedd14f26f2082447d2514c961
selnaggar <selnaggar@e005m04.istic.univ-rennes1.fr>
2023-05-16 13:14:53 +0200

    add go up down right left

--- Jeu2D_ESIR2/src/entity/EntityMovable.java
+++ Jeu2D_ESIR2/src/entity/EntityMovable.java
@@ -9,4 +9,19 @@ public abstract class EntityMovable extends Entity{
 	public GamePanel m_gp;
 	public int m_damage;
 	
+	public void goUp(){
+		m_y += m_speed;
+	}
+	
+	public void goDown() {
+		m_y -= m_speed;
+	}
+	
+	public void goRight() {
+		m_x += m_speed;
+	}
+	
+	public void goLeft() {
+		m_x -= m_speed;
+	}
 }
