549bfe468794aa02011e10d441dc7f270fbb07cc
selnaggar <selnaggar@e005m04.istic.univ-rennes1.fr>
2023-05-16 13:10:22 +0200

    Entity Rearrangement 2

---
+++ Jeu2D_ESIR2/src/entity/EntityMovable.java
@@ -0,0 +1,12 @@
+package entity;
+
+import main.GamePanel;
+
+public abstract class EntityMovable extends Entity{
+
+	public int m_speed;					//D�placement de l'entit�
+	public int m_pv;
+	public GamePanel m_gp;
+	public int m_damage;
+	
+}
